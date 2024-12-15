7aecc8849d08d75decd5be32c4c3317df5cacb8a
Bibek Shrestha <rawseek@gmail.com>
2024-12-15 23:08:18 +1100

    Add appsettings for Test. Update README.md

--- test/BookingApplication.Test/Infrastructure/Helpers/BookingConfigOptionHelper.cs
+++ test/BookingApplication.Test/Infrastructure/Helpers/BookingConfigOptionHelper.cs
@@ -9,7 +9,8 @@ public class BookingConfigOptionHelper
 {
     public static IOptions<BookingConfigOption> configureBookingOption()
     {
-        var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        var environment = "Test";
+        var configuration = new ConfigurationBuilder().AddJsonFile($"appsettings.{environment}.json").Build();
         var bookingConfigOption = new BookingConfigOption();
         configuration.GetSection(BookingConfigOption.BookingConfig).Bind(bookingConfigOption);
         var options = Options.Create(bookingConfigOption);
